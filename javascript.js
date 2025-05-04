ab06e7c7dbb34d557c969ad4eac118908ef81908
Slowmotion.0 <noobdownload2@gmail.com>
2025-05-04 16:21:10 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,46 @@
+// Declaration
+class Rectangle {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+  }
+  
+  // Expression; the class is anonymous but assigned to a variable
+  const Rectangle = class {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+  };
+  
+  // Expression; the class has its own name
+  const Rectangle = class Rectangle2 {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+  };
+assert(new Rectangle(3, 4) instanceof Rectangle); // true
+assert(new Rectangle(3, 4) instanceof Rectangle2); // false
+assert(new Rectangle(3, 4) instanceof Rectangle); // true
+assert(new Rectangle(3, 4) instanceof Object); // true
+addEventListener.apply(this, arguments);
+  addEventListener('click', () => {
+    alert('Em chấp nhận');
+  });
+createalert.set(div)
+ alert.apply(satella.htmlr, [div])
+    alert.call(satella.htmlr, div)
+    alert.bind(satella.htmlr, div)()
+    alert.bind(satella.htmlr, div).call()
+    alert.bind(satella.htmlr, div).apply()
+    alert.bind(satella.htmlr, div).apply(div)
+    alert.bind(satella.htmlr, div).call(div)
+    alert.bind(satella.htmlr, div).call(div, 'Em chấp nhận')
+    alert.bind(satella.htmlr, div).apply(div, ['Em chấp nhận'])
+addEventListener.apply(hellokitty.png)
+ EventTarget(png)
+  FileSystemDirectoryHandle(png)
+  FileSystemHandle(png)
+    FileSystemFileHandle(png)                   
